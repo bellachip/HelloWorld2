9800265164028b4be36eaf3e36d3ebf27438e6bf
Bella Yang <hwy4012@gmail.com>
2019-09-18 01:00:55 -0400

    Committed changes: - edited constraints for button and textview - added a function that changes the text in the textview when the button is clicked - added an id to the textview

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package edu.temple.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void buttonClick(View v) {
+        TextView textview = (TextView)findViewById(R.id.text);
+        textview.setText("You have clicked the button");
     }
 }
 
